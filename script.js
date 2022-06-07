0daff87d28b1d9998fb1ad094eb6007c5fbfa07a
Ism <example@gmail.com>
2022-06-07 14:56:24 +0500

    secind commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const arr = [];
+
+let firsName = prompt("Ismingizni kiriting", '';)
+let lastName = prompt("Familiyangizni kiriting", '';)
+let age = prompt("Yoshingizni kiriting", '';)
+
+console.log(arr[firsName, lastName, age]);
